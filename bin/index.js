2c14b7dbbb77b9dfe2329a0aa0a3448ae775067c
zhise <69071976+zhise@users.noreply.github.com>
2020-08-01 17:32:09 +0800

    Add files via upload

---
+++ bin/index.js
@@ -0,0 +1,32 @@
+/**
+ * 设置LayaNative屏幕方向，可设置以下值
+ * landscape           横屏
+ * portrait            竖屏
+ * sensor_landscape    横屏(双方向)
+ * sensor_portrait     竖屏(双方向)
+ */
+
+window.screenOrientation = "sensor_landscape";
+
+//-----libs-begin-----
+loadLib("libs/laya.core.js")
+loadLib("libs/laya.ui.js")
+loadLib("libs/laya.d3.js")
+loadLib("libs/domparserinone.js")
+loadLib("libs/bytebuffer.js")
+//-----libs-end-------
+loadLib("adapter/zs.laya.wx.conf.js");
+loadLib('adapter/zs.laya.wx.ad.js');
+if (typeof wx !== "undefined") {
+    loadLib('adapter/zs.laya.wx.js');
+    loadLib('adapter/ald-game.js');
+    loadLib('adapter/tdweapp.js');
+    loadLib('adapter/h.js');
+}
+loadLib("framework/zs.laya.js")
+loadLib("framework/zs.laya.ui.js")
+loadLib("framework/zs.laya.game.js")
+loadLib("framework/zs.laya.opendata.js")
+loadLib("framework/zs.laya.tdapp.js")
+loadLib("framework/zs.laya.zsapp.js")
+loadLib("js/bundle.js");
